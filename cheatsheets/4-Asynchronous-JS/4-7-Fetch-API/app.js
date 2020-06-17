ca6316160907adf35264ba98c43f07f995d454d3
Atharva Kulkarni <atharvaak66@gmail.com>
2020-06-17 18:24:42 +0530

    update es6 functions

--- cheatsheets/4-Asynchronous-JS/4-7-Fetch-API/app.js
+++ cheatsheets/4-Asynchronous-JS/4-7-Fetch-API/app.js
@@ -7,25 +7,21 @@ document.getElementById("button3").addEventListener("click", getExternal);
 // Get local text file data
 function getText() {
   fetch("test.txt")
-    .then(function (res) {
-      return res.text();
-    })
-    .then(function (data) {
+    .then((res) => res.text())
+    .then((data) => {
       console.log(data);
       document.getElementById("output").innerHTML = data;
     })
-    .catch(function (err) {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 }
 
 // Get local json data
 function getJson() {
   fetch("posts.json")
-    .then(function (res) {
+    .then((res) => {
       return res.json();
     })
-    .then(function (data) {
+    .then((data) => {
       console.log(data);
       let output = "";
       data.forEach(function (post) {
@@ -33,7 +29,7 @@ function getJson() {
       });
       document.getElementById("output").innerHTML = output;
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.log(err);
     });
 }
