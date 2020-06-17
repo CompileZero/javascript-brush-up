0eb77ec7980bfb0dec7c67f6f066b4d147fd4ec1
Atharva Kulkarni <atharvaak66@gmail.com>
2020-06-17 18:15:10 +0530

    update error handling Fetch

--- cheatsheets/4-Asynchronous-JS/4-7-Fetch-API/app.js
+++ cheatsheets/4-Asynchronous-JS/4-7-Fetch-API/app.js
@@ -1,60 +1,86 @@
-document.getElementById('button1').addEventListener('click', getText);
+document.getElementById("button1").addEventListener("click", getText);
 
-document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById("button2").addEventListener("click", getJson);
 
-document.getElementById('button3').addEventListener('click', getExternal);
+document.getElementById("button3").addEventListener("click", getExternal);
 
 // Get local text file data
 function getText() {
-  fetch('test.txt')
-    .then(function(res){
+  fetch("test.txt")
+    .then(function (res) {
       return res.text();
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      document.getElementById('output').innerHTML = data;
+      document.getElementById("output").innerHTML = data;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
     });
 }
-
 
 // Get local json data
 function getJson() {
-  fetch('posts.json')
-    .then(function(res){
+  fetch("posts.json")
+    .then(function (res) {
       return res.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      let output = '';
-      data.forEach(function(post) {
+      let output = "";
+      data.forEach(function (post) {
         output += `<li>${post.title}</li>`;
       });
-      document.getElementById('output').innerHTML = output;
+      document.getElementById("output").innerHTML = output;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
     });
 }
-
 
 // Get from external API
 function getExternal() {
-  fetch('https://api.github.com/users')
-    .then(function(res){
+  fetch("https://api.github.com/users")
+    .then(function (res) {
       return res.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      let output = '';
-      data.forEach(function(user) {
+      let output = "";
+      data.forEach(function (user) {
         output += `<li>${user.login}</li>`;
       });
-      document.getElementById('output').innerHTML = output;
+      document.getElementById("output").innerHTML = output;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
     });
 }
+
+/*
+Error Handling With Fetch
+I did not mention this in the lecture but error handling with fetch is a bit different than with something like Axios or jQuery. If there is an http error, it will not fire off .catch automatically. You have to check the response and throw an error yourself. Here is an example....
+fetch('https://devcamper.io/api/v1/bootcamps/34343')
+  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) {
+       throw new Error(res.error);
+    }
+    return res;
+  })
+  .catch(err => console.log(err));
+ 
+I would suggest creating a separate function for error handling
+
+function handleErrors(res) {
+  if (!res.ok) throw new Error(res.error);
+  return res;
+}
+ 
+fetch('https://devcamper.io/api/v1/bootcamps/34343')
+  .then(res => res.json())
+  .then(handleErrors)
+  .then(res => console.log(res.data))
+  .catch(err => console.log(err));
+ 
+*/
